0bf788dee891e935bbbdd0784584495536abe260
marina belikova <memymilk@gmail.com>
2023-03-08 00:38:11 +0100

    Setting up bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "profile", to: "pages#profile", as: :profile
+  get "feedback", to: "pages#feedback/:id"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
