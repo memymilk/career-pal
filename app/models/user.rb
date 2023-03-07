05083ca37f5a7300f446ea1f2725118e21c282c3
marina belikova <memymilk@gmail.com>
2023-03-07 12:24:34 +0100

    Srart

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :video_calls, class_name: "VideoCall", foreign_key: :user_one
+  has_many :video_calls, class_name: "VideoCall", foreign_key: :user_two
 end
