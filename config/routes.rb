382d9e3260c66f5291eb3734d0b337e5f0f23cb2
marina belikova <memymilk@gmail.com>
2023-03-07 17:16:11 +0100

    new fields for feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "profile", to: "pages#profile", as: :profile
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
