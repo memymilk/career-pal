f9295ccbc823b57196ee07cba38f13ffa0bcbf69
Giulia Zeni <giuli.zeni@gmail.com>
2023-03-08 10:14:56 +0100

    seed fix

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,4 @@
 class Feedback < ApplicationRecord
   belongs_to :giver, class_name: "User"
-  belongs_to :receiver, class_name: "User"
   belongs_to :video_call, class_name: "VideoCall"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
   resources :video_call do
     resources :feedback, only: %i[new create]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   get "profile", to: "pages#profile", as: :profile
 end
