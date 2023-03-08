8dcbaec43107b45e97d9fe61731c91c9a198861d
marina belikova <memymilk@gmail.com>
2023-03-08 13:03:42 +0100

    seeds fixed with dependencies now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,14 @@ class PagesController < ApplicationController
 
   def user_show
   end
+
+  def profile
+    @my_array = []
+    user = User.last
+    user.video_calls.each do |videocall|
+      videocall.feedbacks.each do |feedback|
+        @my_array << feedback if feedback.giver_id != user.id
+      end
+    end
+  end
 end
