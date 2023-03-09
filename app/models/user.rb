c1937ef6b2cce5aae58bf20cec49d0e0174441fd
Lukas Schanderl <lukas@veecoco.com>
2023-03-09 12:36:57 +0100

    lukas form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :video_calls, class_name: "VideoCall", foreign_key: :user_one, dependent: :destroy
-  has_many :video_calls, class_name: "VideoCall", foreign_key: :user_two, dependent: :destroy
+  has_many :videocalls, class_name: "VideoCall", foreign_key: :user_one, dependent: :destroy
+  has_many :videocalls, class_name: "VideoCall", foreign_key: :user_two, dependent: :destroy
   # has_many :feedbacks_as_giver, class_name: "Feedback", foreign_key: :giver_id
   # has_many :feedbacks_as_receiver, class_name: "Feedback", foreign_key: :receiver_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-<<<<<<< HEAD
   resources :videocalls, only: :show
-=======
 
   get "profile", to: "pages#profile", as: :profile
   # get "feedback", to: "pages#feedback/:id"
 
   resources :users do
-    resources :video_call
+    resources :videocalls
   end
 
 
-  resources :video_call do
-    resources :feedback, only: %i[new create show]
+  resources :videocalls do
+    resources :feedbacks, only: %i[new create show]
   end
 
 
->>>>>>> a56798053b9a21251747f34003d6e5b430712f96
 end
