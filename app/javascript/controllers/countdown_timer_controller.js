e6b67bc337012f518e83344ea43c93a1c70cb358
Lukas Schanderl <lukas@veecoco.com>
2023-03-13 12:22:15 +0100

    added some gems for xing

--- app/javascript/controllers/countdown_timer_controller.js
+++ app/javascript/controllers/countdown_timer_controller.js
@@ -5,27 +5,27 @@ export default class extends Controller {
 
   connect() {
     console.log("test")
-    this.startTime = new Date();
-    this.endTime = new Date(this.startTime.getTime() + 30 * 60 * 1000); // 30 minutes in milliseconds
-    console.log("test")
-    this.timer = setInterval(() => {
-      const currentTime = new Date();
-      const timeLeft = this.endTime - currentTime;
-      if (timeLeft < 0) {
-        clearInterval(this.timer);
-        this.timeTarget.innerHTML = "Time is up!";
-      } else {
-        const minutes = Math.floor(timeLeft / 1000 / 60);
-        let seconds = Math.floor((timeLeft / 1000) % 60);
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        this.timeTarget.innerHTML = `${minutes}:${seconds}`;
-      }
-    }, 1000);
-  }
+  //   this.startTime = new Date();
+  //   this.endTime = new Date(this.startTime.getTime() + 30 * 60 * 1000); // 30 minutes in milliseconds
+  //   console.log("test")
+  //   this.timer = setInterval(() => {
+  //     const currentTime = new Date();
+  //     const timeLeft = this.endTime - currentTime;
+  //     if (timeLeft < 0) {
+  //       clearInterval(this.timer);
+  //       this.timeTarget.innerHTML = "Time is up!";
+  //     } else {
+  //       const minutes = Math.floor(timeLeft / 1000 / 60);
+  //       let seconds = Math.floor((timeLeft / 1000) % 60);
+  //       seconds = seconds < 10 ? "0" + seconds : seconds;
+  //       this.timeTarget.innerHTML = `${minutes}:${seconds}`;
+  //     }
+  //   }, 1000);
+  // }
 
-  disconnect() {
-    clearInterval(this.timer);
-  }
+  // disconnect() {
+  //   clearInterval(this.timer);
+  // }
 }
 
 
