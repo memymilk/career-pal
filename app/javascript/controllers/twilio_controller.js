5851adc2ab6ec22fb4a648a88bf58c1d351c7a71
Lukas Schanderl <lukas@veecoco.com>
2023-03-14 09:19:54 +0100

    twilio controller

--- app/javascript/controllers/twilio_controller.js
+++ app/javascript/controllers/twilio_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 const { connect, createLocalVideoTrack, createLocalTracks } = require('twilio-video');
 
-
 // Connects to data-controller="twilio"
 export default class extends Controller {
 
