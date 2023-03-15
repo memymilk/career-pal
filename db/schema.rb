736136277694db26fbbecb32e1e31971f71e6ad2
Antje Stiller <antjestiller@gmx.de>
2023-03-15 14:22:47 +0100

    switch roles

--- db/schema.rb
+++ db/schema.rb
@@ -72,11 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_093649) do
     t.index ["videocall_id"], name: "index_feedbacks_on_videocall_id"
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "questions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
